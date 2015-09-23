2a42eb1da76c6d734bba7479b8789e715996288f
David Smith <davidflsmith@gmail.com>
2015-09-23 11:50:05 -0700

    Initial Upload
    My Favorite Movies Website

---
+++ media.py
@@ -0,0 +1,27 @@
+# file media.py
+
+import webbrowser
+
+
+# define video class
+class Video():
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+
+# define movie class
+class Movie(Video):
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, movie_title, movie_rating, movie_stars,
+                 movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.rating = movie_rating
+        self.stars = movie_stars
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
